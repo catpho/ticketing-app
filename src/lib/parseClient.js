08bae6c1f011671c722a161f482958af89ba34a2
Cathy Phan <cathyp98@outlook.com>
2026-03-01 22:33:41 -0500

    yay

--- src/lib/parseClient.js
+++ src/lib/parseClient.js
@@ -1,7 +1,7 @@
 import Parse from 'parse';
 
 // Initialize Parse SDK
-Parse.initialize(process.env.YOUR_PARSE_APP_ID, process.env.YOUR_PARSE_JS_KEY);
+Parse.initialize(process.env.NEXT_PUBLIC_PARSE_APP_ID, process.env.NEXT_PUBLIC_PARSE_JS_KEY);
 Parse.serverURL = 'https://parseapi.back4app.com';
 
 export default Parse;
